c138676364355c29a79e1a20dcfa2899d1f4344e
sanvo <marco.sanvoisin@educa.madrid.org>
2025-02-14 10:06:40 +0100

    V1

---
+++ src/Lista.java
@@ -0,0 +1,61 @@
+
+import java.util.Arrays;
+
+public class Lista {
+
+    Integer lista[];
+
+    public Lista() {
+        lista = new Integer[0];
+    }
+
+    public int getNumElementos() {
+        return lista.length;
+    }
+
+    public void insertarFinal(Integer num) {
+
+        Integer t[] = Arrays.copyOf(lista, lista.length + 1);
+        t[t.length - 1] = num;
+        lista = t;
+    }
+
+    public void insertarPrincipio(Integer num) {
+        Integer t[] = new Integer[lista.length + 1];
+        System.arraycopy(lista, 0, t, 1, lista.length);
+        t[0] = num;
+        lista = t;
+    }
+
+    public void insertar(int pos, Integer num) {
+        if (pos > lista.length || pos < 0) {
+            System.out.println("No se puede insertar en esa posicion");
+        } else {
+            Integer t[] = new Integer[lista.length + 1];
+            System.arraycopy(lista, 0, t, 0, pos);
+            System.arraycopy(lista, pos, t, pos + 1, lista.length - pos);
+            t[pos] = num;
+            lista = t;
+        }
+    }
+    
+    public void insertarArray(Integer t1[]){
+        Integer t2[] = new Integer[lista.length + t1.length];
+        System.arraycopy(lista, 0, t2, 0, lista.length);
+        System.arraycopy(t1, 0, t2, lista.length, t1.length );
+        lista = t2;
+    }
+    
+    public void eliminarElemento(int pos){ //TODO: Arreglar el ultimo arrayCopy que no va.
+        Integer t[] = new Integer[lista.length -1];
+        System.arraycopy(lista, 0, t, 0, pos - 1);
+        System.out.println(Arrays.toString(t));
+        System.arraycopy(lista, pos - 1, t, pos, lista.length - pos);
+        lista = t;
+    }
+
+    public void mostrarLista() {
+        System.out.println(Arrays.toString(lista));
+    }
+
+}
---
+++ src/pruebaLista.java
@@ -0,0 +1,23 @@
+
+
+
+public class pruebaLista {
+    public static void main(String[] args) {
+        Lista l = new Lista();
+        l.insertarFinal(4);
+        l.insertarFinal(7);
+        l.insertarPrincipio(8);
+        l.insertar(2, 5);
+        l.insertar(3,9);
+        l.insertar(5,19);
+        l.mostrarLista();
+        
+        Integer t[] = {1,2,3,4,5,6};
+        l.insertarArray(t);
+        l.mostrarLista();
+        
+        l.eliminarElemento(3);
+        l.mostrarLista();
+        
+    }
+}
